1b61320ced4080d4c49c80aa32185dbce6c3dbf3
pavelkin <pavel.kingsep@gmail.com>
2016-02-28 16:42:24 +0300

    update dist and version

--- dist/origin/video.js
+++ dist/origin/video.js
@@ -1,6 +1,6 @@
 /**
  * @license
- * Video.js 5.0.2-15 <http://videojs.com/>
+ * Video.js 5.0.2-16 <http://videojs.com/>
  * Copyright Brightcove, Inc. <https://www.brightcove.com/>
  * Available under Apache License Version 2.0
  * <https://github.com/videojs/video.js/blob/master/LICENSE>
@@ -17440,7 +17440,7 @@ setup.autoSetupTimeout(1, videojs);
  *
  * @type {String}
  */
-videojs.VERSION = '5.0.2-15';
+videojs.VERSION = '5.0.2-16';
 
 /**
  * The global options object. These are the settings that take effect
