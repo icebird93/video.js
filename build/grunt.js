2c2e4274f1ec32293df0cb0656c1a68797d73788
Bahaa Aidi <bahaa.aidi@gmail.com>
2016-04-14 10:41:42 +0300

    update default swf url

--- build/grunt.js
+++ build/grunt.js
@@ -327,7 +327,7 @@ module.exports = function(grunt) {
           ['browserify-versionify', {
             placeholder: '__SWF_VERSION__',
             version: pkg.dependencies['videojs-swf'].match(
-                /git\+https:\/\/github\.com\/hola\/video-js-swf-sv#v(.*)-\d+$/)[1]
+                /git\+https:\/\/github\.com\/hola\/video-js-swf-sv#(.+)$/)[1]
           }]
         ]
       },
--- src/js/tech/flash.js
+++ src/js/tech/flash.js
@@ -74,7 +74,7 @@ class Flash extends Tech {
     // Otherwise this adds a CDN url.
     // The CDN also auto-adds a swf URL for that specific version.
     if (!options.swf) {
-      options.swf = '//vjs.zencdn.net/swf/__SWF_VERSION__/video-js.swf';
+      options.swf = '//cdn.rawgit.com/hola/video-js-swf-sv/__SWF_VERSION__/dist/video-js.swf';
     }
 
     // Generate ID for swf object
