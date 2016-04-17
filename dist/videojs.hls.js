d665864dd5c7bc5d702fe4dc24b8397b3d4fcfcc
Bahaa Aidi <bahaa.aidi@gmail.com>
2016-04-17 16:47:04 +0300

    update dist and version (v5.0.2-30)

--- dist/videojs.hls.js
+++ dist/videojs.hls.js
@@ -1,6 +1,6 @@
 /**
  * @license
- * Video.js 5.0.2-29 <http://videojs.com/>
+ * Video.js 5.0.2-30 <http://videojs.com/>
  * Copyright Brightcove, Inc. <https://www.brightcove.com/>
  * Available under Apache License Version 2.0
  * <https://github.com/videojs/video.js/blob/master/LICENSE>
@@ -11906,7 +11906,7 @@ var Flash = (function (_Tech) {
     // Otherwise this adds a CDN url.
     // The CDN also auto-adds a swf URL for that specific version.
     if (!options.swf) {
-      options.swf = '//cdn.rawgit.com/hola/video-js-swf-sv/v5.0.1-6/dist/video-js.swf';
+      options.swf = '//cdn.rawgit.com/hola/video-js-swf-sv/v5.0.1-7/dist/video-js.swf';
     }
 
     // Generate ID for swf object
@@ -17440,7 +17440,7 @@ setup.autoSetupTimeout(1, videojs);
  *
  * @type {String}
  */
-videojs.VERSION = '5.0.2-29';
+videojs.VERSION = '5.0.2-30';
 
 /**
  * The global options object. These are the settings that take effect
