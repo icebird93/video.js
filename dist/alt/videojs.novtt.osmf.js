4eedc1a9952b715dda9ea8434f007190e209113f
norlin <alexey@norlin.guru>
2016-05-10 12:32:40 +0300

    Update build

--- dist/alt/videojs.novtt.osmf.js
+++ dist/alt/videojs.novtt.osmf.js
@@ -18275,7 +18275,7 @@ HolaSkin.prototype.dispose = function(){
 var defaults = {
     className: 'vjs5-hola-skin',
     css: '/css/videojs-hola-skin.css',
-    ver: 'ver=0.0.2-20'
+    ver: 'ver=0.0.2-21'
 };
 
 // VideoJS plugin register
