00c7c381581a1bded19ead5266f65d1b284069b6
norlin <alexey@norlin.guru>
2016-05-18 16:51:47 +0300

    fix flash merge issues

--- build/grunt.js
+++ build/grunt.js
@@ -37,7 +37,8 @@ module.exports = function(grunt) {
       }],
       ['browserify-versionify', {
         placeholder: '__SWF_VERSION__',
-        version: pkg.dependencies['videojs-swf']
+        version: pkg.dependencies['videojs-swf'].match(
+          /git\+https:\/\/github\.com\/hola\/video-js-swf-sv#(.+)$/)[1]
       }]
     ]
   };
