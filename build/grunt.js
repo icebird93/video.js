5809c49533fce20e042e26cc7f499b12d055ad0e
norlin <alexey@norlin.guru>
2016-05-23 13:01:27 +0300

    update dependencies

--- build/grunt.js
+++ build/grunt.js
@@ -38,7 +38,7 @@ module.exports = function(grunt) {
       ['browserify-versionify', {
         placeholder: '__SWF_VERSION__',
         version: pkg.dependencies['videojs-swf'].match(
-          /git\+https:\/\/github\.com\/hola\/video-js-swf-sv#(.+)$/)[1]
+          /hola\/video-js-swf-sv#(.+)$/)[1]
       }]
     ]
   };
@@ -410,7 +410,7 @@ module.exports = function(grunt) {
           ['browserify-versionify', {
             placeholder: '__SWF_VERSION__',
             version: pkg.dependencies['videojs-swf'].match(
-                /git\+https:\/\/github\.com\/hola\/video-js-swf-sv#(.+)$/)[1]
+                /hola\/video-js-swf-sv#(.+)$/)[1]
           }]
         ]
       },
